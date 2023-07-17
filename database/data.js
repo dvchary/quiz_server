7a66bf5e7c182546dda257a0f07ec7d2bdc1b0da
dvchary <varaprasad.achary@gmail.com>
2023-07-17 14:40:30 +0530

    Changes

--- database/data.js
+++ database/data.js
@@ -1,49 +1,32 @@
 export default [
-    {
-        id: 1,
-        question : "Javascript is an _______ language",
-        options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
-        ]
-    },
-    {
-        id: 2,
-        question : "Following methods can be used to display data in some form using Javascript",
-        options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
-        ]
-    },
-    {
-        id: 3,
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
-        options : [
-            'Boolean',
-            'Undefined',
-            'Object',
-        ]
-    },
-    {
-        id: 4,
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer'
-        ]
-    },
-    {
-        id: 5,
-        question : "Which function is used to serialize an object into a JSON string?",
-        options : [
-            'stringify()',
-            'parse()',
-            'convert()',
-        ]
-    }
+  {
+    id: 1,
+    question: 'How many annual holidays does DataFactZ have in a year?',
+    options: ['10 Days', '11 Days', '9 Days'],
+  },
+  {
+    id: 2,
+    question:
+      'What is the maximum number of earned leaves that an employee can avail in one year?',
+    options: ['15 Days', '20 Days', '22 Days'],
+  },
+  {
+    id: 3,
+    question: 'How many work-from-home days is an employee eligible to avail?',
+    options: ['15 Days', '11 Days', '20 Days'],
+  },
+  {
+    id: 4,
+    question:
+      'What is the coverage amount of our health insurance policy for a family?',
+    options: ['3 Lakhs', '2.5 Lakhs', '2 Lakhs'],
+  },
+  {
+    id: 5,
+    question:
+      'Which tool does DFZ use to request and manage leaves and work-from-home requests?',
+    options: ['BambooHR', 'Workday', 'Kronos'],
+  },
 ];
 
-export const answers = [0, 1, 2, 1, 0];
+export const answers = [0, 2, 0, 0, 0];
